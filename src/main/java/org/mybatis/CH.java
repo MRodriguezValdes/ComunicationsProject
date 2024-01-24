eabe54dda6a92c0c52baabc07fbcbe2cafa621db
MRodriguezValdes <mrodriguezvaldes@cifpfbmoll.eu>
2024-01-24 18:24:52 +0100

    good entrega

--- src/main/java/org/mybatis/CH.java
+++ src/main/java/org/mybatis/CH.java
@@ -1,40 +1,77 @@
-    package org.mybatis;
+package org.mybatis;
 
-    import java.io.BufferedReader;
-    import java.io.IOException;
-    import java.io.InputStreamReader;
-    import java.io.PrintWriter;
-    import java.net.Socket;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
 
-    public class CH {
-        private Socket socket;
-        private BufferedReader in;
-        private PrintWriter out;
-        private PID peerID;
+public class CH implements Runnable {
+    private Socket socket;
+    private BufferedReader in;
+    private PrintWriter out;
+    private PID peerID;
+    private CCT connectionController;
 
-        public CH(Socket socket, PID peerID) throws IOException {
-            this.socket = socket;
-            this.peerID = peerID;
+    public CH(Socket socket, PID peerID, CCT connectionController) throws IOException {
+        this.socket = socket;
+        this.peerID = peerID;
+        this.connectionController = connectionController; // Agregar esta línea
 
-            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            out = new PrintWriter(socket.getOutputStream(), true); // Configurar autoFlush
-        }
+        in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        out = new PrintWriter(socket.getOutputStream(), true); // Configurar autoFlush
+    }
 
-        public void sendMessage(String message) {
-            out.println(message);
-        }
+    public void sendMessage(String message) {
+        out.println(message);
+    }
 
-        public String receiveMessage() throws IOException {
-            return in.readLine();
-        }
+    public String receiveMessage() throws IOException {
+        return in.readLine();
+    }
 
-        public void close() throws IOException {
-            in.close();
-            out.close();
-            socket.close();
-        }
+    public void close() throws IOException {
+        in.close();
+        out.close();
+        socket.close();
+    }
 
-        public PID getPeerID() {
-            return peerID;
+    public PID getPeerID() {
+        return peerID;
+    }
+
+    private void handleInput() {
+        try (Scanner scanner = new Scanner(System.in)) {
+            while (true) {
+                if (scanner.hasNextLine()) {
+                    String inputMessage = scanner.nextLine();
+                    this.sendMessage(inputMessage);
+                }
+            }
         }
     }
+
+    @Override
+    public void run() {
+        new Thread(this::handleInput).start();
+        try {
+            while (true) {
+                String message = receiveMessage();
+                if (message != null) {
+                    System.out.println(peerID.getPeerName() + ": " + message);
+                }
+            }
+        } catch (IOException e) {
+            // Manejar desconexión
+            System.out.println("Se desconectó: " + peerID.getPeerName());
+            try {
+                close();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+            connectionController.getChannels().remove(this);
+        }
+
+    }
+}
--- src/main/java/org/mybatis/SC.java
+++ src/main/java/org/mybatis/SC.java
@@ -3,16 +3,14 @@ package org.mybatis;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.ArrayList;
-import java.util.List;
 
 public class SC implements Runnable {
     private ServerSocket serverSocket;
     private CCT connectionController;
 
-    public SC(int port,CCT connectionController) throws IOException {
+    public SC(int port, CCT connectionController) throws IOException {
         serverSocket = new ServerSocket(port);
-        this.connectionController= connectionController;
+        this.connectionController = connectionController;
     }
 
     @Override
@@ -22,40 +20,15 @@ public class SC implements Runnable {
                 Socket clientSocket = serverSocket.accept();
                 System.out.println("connected " + clientSocket.getInetAddress().getHostAddress());
                 PID peerID = new PID("Peer" + connectionController.getChannels().size(), clientSocket.getInetAddress().getHostAddress(), clientSocket.getPort());
-                CH channel = new CH(clientSocket, peerID);
+                CH channel = new CH(clientSocket, peerID,connectionController);
                 connectionController.getChannels().add(channel);
-                // Start a HealthChecker thread for the new channel
-//                HC healthChecker = new HC(channel);
-//                new Thread(healthChecker).start();
+                // todo Start a HealthChecker thread for the new channel
 
                 // Handle messages from this channel in a new thread
-                new Thread(() -> {
-                    try {
-                        handleMessages(channel);
-                    } catch (IOException e) {
-                        throw new RuntimeException(e);
-                    }
-                }).start();
+                new Thread(channel).start();
             }
         } catch (IOException e) {
             e.printStackTrace();
-        }
-    }
-
-    private void handleMessages(CH channel) throws IOException {
-        try {
-            while (true) {
-                String message = channel.receiveMessage();
-                if (message!=null){
-                    System.out.println("Recibo como servidor");
-                    System.out.println(channel.getPeerID().getPeerName() + ": " + message);
-                }
-            }
-        } catch (IOException e) {
-            // Handle disconnection
-            System.out.println("adios chanel");
-            channel.close();
-            connectionController.getChannels().remove(channel);
         }
     }
 }
